d7fc264612d21b92fa6a9b25d3b9391e9b88b1b0
Bitsulov <roma.bitsulov@mail.ru>
2026-06-05 02:01:39 +0300

    add BeanConfig and AuthController
    Added ModelMapper and CORS configuration bean.
    Added empty AuthController mapped to /api/auth.

---
+++ src/main/java/ru/vibeart/api/controllers/AuthController.java
@@ -0,0 +1,16 @@
+package ru.vibeart.api.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+
+    public AuthController() {
+
+    }
+
+
+}
