7e992bd6ce0ad22ed319e589981aeeaad25188fb
Bitsulov <roma.bitsulov@mail.ru>
2026-07-02 15:37:24 +0300

    change param in loadUserByUsername from email to UUID, email can be used too

--- src/main/java/ru/vibeart/api/services/impl/CustomUserDetailsService.java
+++ src/main/java/ru/vibeart/api/services/impl/CustomUserDetailsService.java
@@ -8,6 +8,8 @@ import ru.vibeart.api.models.entities.User;
 import ru.vibeart.api.repositories.UserRepository;
 import ru.vibeart.api.security.UserPrincipal;
 
+import java.util.UUID;
+
 /**
  * Реализация {@link UserDetailsService}, используемая Spring Security
  * для загрузки данных пользователя по email (username).
@@ -39,21 +41,43 @@ public class CustomUserDetailsService implements UserDetailsService {
     }
 
     /**
-     * Загружает пользователя по email (используется как username).
+     * Загружает пользователя по UUID или email.
      * <p>
      * Если пользователь найден, преобразует его в {@link UserPrincipal}.
      * Если нет — выбрасывает {@link UsernameNotFoundException}.
      * </p>
      *
-     * @param email email пользователя
+     * @param uuid уникальный идентификатор пользователя
      * @return {@link UserDetails} — объект, совместимый со Spring Security
      * @throws UsernameNotFoundException если пользователь не найден
      */
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+    public UserDetails loadUserByUsername(String uuid) throws UsernameNotFoundException {
+        User user;
+
+        if(isUuid(uuid)) {
+            user = userRepository.findByUuid(UUID.fromString(uuid))
+                    .orElseThrow(() -> new UsernameNotFoundException("User not found with uuid: " + uuid));
+        } else {
+            user = userRepository.findByEmail(uuid)
+                    .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + uuid));
+        }
 
         return UserPrincipal.create(user);
+    }
+
+    /**
+     * Проверяет, является ли переданная строка UUID.
+     *
+     * @param str проверяемая строка
+     * @return {@code boolean}
+     */
+    private boolean isUuid(String str) {
+        try {
+            UUID.fromString(str);
+            return true;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
     }
 }
